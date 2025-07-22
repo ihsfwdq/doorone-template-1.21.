8a7cff1554941667d0f9a79dd3b7097949007bd1
ihsfwdq <dhruvpat044@gmail.com>
2025-07-22 18:21:29 -0400

    My first minecraft mod

---
+++ src/main/java/net/me/doorone/DooroneClient.java
@@ -0,0 +1,10 @@
+package net.me.doorone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DooroneClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
